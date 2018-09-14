f944696d710b2add22b877174b1f04bb3c326ae5
Flourishing827 <huangxiangrong827@163.com>
2018-09-14 20:49:38 +0800

    github webhook test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,7 @@
                 <div class="title m-b-md">
                     稍等一下.....
                 </div>
+                <h4>最新功能即将上线</h4>
 
                 <hr>
                 <div class="links">
