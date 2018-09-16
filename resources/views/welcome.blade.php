67900a16944c5509bdb61c4cdabb817c19889dd6
Flourishing827 <huangxiangrong827@163.com>
2018-09-17 00:07:27 +0800

    something fot wechat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HELLOHXR.CN</title>
+        <title>Very little is needed to make a happy life.</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
