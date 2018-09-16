ee0cf30c5cfe20af8c563272318d6b2193d935d3
Flourishing827 <huangxiangrong827@163.com>
2018-09-16 14:53:20 +0800

    something fot wechat

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class DeploymentController extends Controller
 {
     public function deploy(Request $request)
     {
-        Log::info(system("whoami")) ;
         $commands = ['cd /var/www/my-site', 'git pull'];
         $signature = $request->header('X-Hub-Signature');
         $payload = file_get_contents('php://input');
         if ($this->isFromGithub($payload, $signature)) {
             foreach ($commands as $command) {
-               Log::info(shell_exec($command)) ;
+               shell_exec($command);
             }
             http_response_code(200);
         } else {
