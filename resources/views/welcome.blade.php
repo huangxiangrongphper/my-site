49ce22c10ddb1a9a7217f587b4f4d1e9b42d4d81
Flourishing827 <huangxiangrong827@163.com>
2018-09-16 11:27:29 +0800

    something fot wechat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
                 <div class="title m-b-md">
                     稍等一下.....
                 </div>
-                <h4>新功能即将上线中</h4>
+                <h4>新功能即将上线</h4>
             </div>
         </div>
         <hr>
