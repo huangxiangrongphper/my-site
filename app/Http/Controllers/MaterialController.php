d7d6cf0d5cef6a74bbfde764d2772f394e40e8ba
Flourishing827 <huangxiangrong827@163.com>
2018-09-16 23:15:24 +0800

    something fot wechat

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -10,8 +10,8 @@ class MaterialController extends Controller
 
     public function image()
     {
-        $officialAccount = Factory::officialAccount();
-        $image = $officialAccount->material->uploadImage(public_path().'/images/qrcode_for_gh_c25035a188af_258.jpg');
+        $app = app('wechat.official_account');
+        $image = $app->material->uploadImage(public_path().'/images/qrcode_for_gh_c25035a188af_258.jpg');
 
         return $image;
     }
