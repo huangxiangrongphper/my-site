3ecfb7ba4f3af5814e2900ba24252cca098e73ce
Flourishing827 <huangxiangrong827@163.com>
2018-09-16 23:08:52 +0800

    something fot wechat

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 
-use EasyWeChatComposer\EasyWeChat;
+use EasyWeChat\Factory;
 
 class MaterialController extends Controller
 {
 
     public function image()
     {
-        $officialAccount = EasyWeChat::officialAccount();
+        $officialAccount = Factory::officialAccount();
         $image = $officialAccount->material->uploadImage(public_path().'/images/qrcode_for_gh_c25035a188af_258.jpg');
 
         return $image;
