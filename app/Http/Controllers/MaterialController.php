98874716fbba64576d4dd1d7e60992ee47e9e149
Flourishing827 <huangxiangrong827@163.com>
2018-09-16 22:57:15 +0800

    something fot wechat

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -8,8 +8,8 @@ class MaterialController extends Controller
 
     public function image()
     {
-        $app = app('wechat.official_account');
-        $image = $app->material->uploadImage(public_path().'/images/qrcode_for_gh_c25035a188af_258.jpg');
+        $officialAccount = EasyWeChat::officialAccount();
+        $image = $officialAccount->material->uploadImage(public_path().'/images/qrcode_for_gh_c25035a188af_258.jpg');
 
         return $image;
     }
