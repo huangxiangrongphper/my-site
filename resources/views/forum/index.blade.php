9b90823050ecb38f83c4f7e7551730e2195134a5
Flourishing827 <huangxiangrong827@163.com>
2018-09-19 01:52:52 +0800

    something fot wechat

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -22,7 +22,7 @@
                             <a href="/discussions/{{ $discussion->id }}">{{ $discussion->title }}</a>
                     <div class="media-conversation-meta">
                         <span class="media-conversation-replies">
-                            <a href="/discussion/154#reply">{{ count($discussion->comments) }}</a>
+                            {{--<a href="/discussion/154#reply">{{ count($discussion->comments) }}</a>--}}
                             回复
                         </span>
                     </div>
