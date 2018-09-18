3fde11607f1a8856af6503c23dafe0d1902fd041
Flourishing827 <huangxiangrong827@163.com>
2018-09-19 01:08:34 +0800

    something fot wechat

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('avatar');
+            $table->string('confirm_code',64);
+            $table->integer('is_confirmed')->default(0);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
         });
