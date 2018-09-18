c625f4393177ffec1375a59596c33e9111fad053
Flourishing827 <huangxiangrong827@163.com>
2018-09-19 02:01:31 +0800

    something fot wechat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Discussion</title>
+    <title>hellohxr.cn</title>
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="/css/font-awesome.css">
     <link rel="stylesheet" href="/css/style.css">
