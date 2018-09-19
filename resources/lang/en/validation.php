e405fadd41972c06c04911b65d3e13baaf8267b8
Flourishing827 <huangxiangrong827@163.com>
2018-09-19 13:13:03 +0800

    something fot wechat

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -125,8 +125,23 @@ return [
     */
 
     'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
+        'name' => [
+            'required' => '用户名不能为空',
+            'min'      => '用户名长度不能少于:min位'
+        ],
+        'email' => [
+            'required' => '邮箱不能为空',
+            'email'    => '邮箱格式错误',
+            'unique'   => '邮箱已被注册',
+        ],
+        'password' => [
+            'required' => '密码不能为空',
+            'min'      => '密码长度不能少于:min位',
+            'confirmed'   => '确认密码和密码不一致',
+        ],
+        'password_confirmation' => [
+            'required' => '确认密码不能为空',
+            'min'      => '确认密码长度不能少于:min位',
         ],
     ],
 
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -3,6 +3,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3" role="main">
+                @if($errors->any())
+                    <ul class="list-group">
+                        @foreach($errors->all() as $error)
+                            <li class="list-group-item list-group-item-danger">{{ $error  }}</li>
+                        @endforeach
+                    </ul>
+                @endif
                 <form action="/user/register" method="POST" accept-charset="UTF-8">
                     {{ csrf_field() }}
                     <div class="form-group">
