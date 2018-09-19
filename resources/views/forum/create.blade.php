b2b487ac9471d6c9969a1b5de3f23f2ae1f71ea8
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 01:31:57 +0800

    something fot wechat

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -4,6 +4,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2" role="main">
+                @if($errors->any())
+                    <ul class="list-group">
+                        @foreach($errors->all() as $error)
+                            <li class="list-group-item list-group-item-danger">{{ $error  }}</li>
+                        @endforeach
+                    </ul>
+                @endif
                 <form action="/discussions/store" method="POST" accept-charset="UTF-8">
                     {{csrf_field()}}
                      @include('forum.form')
