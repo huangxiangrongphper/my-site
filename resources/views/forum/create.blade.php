22ef26a70391a120a37475ff47325a80f3ba9f65
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 00:23:15 +0800

    something fot wechat

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 @section('content')
-    @include('editor::head')
+    {{--@include('editor::head')--}}
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1" role="main">
