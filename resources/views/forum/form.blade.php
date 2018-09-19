9ef71b9ea3f2d78fd1cc371d73cc51717e157429
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 00:25:41 +0800

    something fot wechat

--- resources/views/forum/form.blade.php
+++ resources/views/forum/form.blade.php
@@ -1,12 +1,12 @@
 <!-- Title Field -->
 <div class="form-group">
-    <label for="title" class="control-label">标题:</label>
+    <label for="title" >标题:</label>
     <input id="title" name="title"  class="form-control">
 </div>
 <!-- Body Field -->
 <div class="form-group">
     <div class="editor">
-        <label for="body" class="control-label">内容:</label>
+        <label for="body">内容:</label>
         <input id="body" name="body"  class="form-control">
     </div>
 </div>
