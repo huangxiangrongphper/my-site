9db1eaff7ee7d18d0fb3095ff1663b91d3f7f973
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 01:29:01 +0800

    something fot wechat

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2" role="main">
                 <form action="/discussions/store" method="POST" accept-charset="UTF-8">
+                    {{csrf_field()}}
                      @include('forum.form')
                     <div>
                         <button type="submit" class="btn btn-primary pull-right">发表帖子</button>
