883e3eb75a5aa9d25b8adc3291bde86151174edf
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 16:04:54 +0800

    something fot wechat

--- resources/views/forum/edit.blade.php
+++ resources/views/forum/edit.blade.php
@@ -3,6 +3,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2" role="main">
+                @if($errors->any())
+                    <ul class="list-group">
+                        @foreach($errors->all() as $error)
+                            <li class="list-group-item list-group-item-danger">{{ $error  }}</li>
+                        @endforeach
+                    </ul>
+                @endif
                 <form action="/discussions/{{$discussion->id}}/edit"  method="POST" accept-charset="UTF-8">
                     {{csrf_field()}}
                 <div class="form-group">
