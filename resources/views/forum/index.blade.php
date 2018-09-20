226a8a1095dad4bcaab41a2c817a0d118355be04
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 14:28:54 +0800

    something fot wechat

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="media-body">
                         <h4 class="media-heading">
-                            <a href="/discussions/{{ $discussion->id }}">{{ $discussion->title }}</a>
+                            <a href="/discussions/show/{{ $discussion->id }}">{{ $discussion->title }}</a>
                     <div class="media-conversation-meta">
                         <span class="media-conversation-replies">
                             {{--<a href="/discussion/154#reply">{{ count($discussion->comments) }}</a>--}}
