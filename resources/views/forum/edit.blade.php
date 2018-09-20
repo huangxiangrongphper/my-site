eb23000fc3d4fb1e3163263fb9638589d8fe767e
Flourishing827 <huangxiangrong827@163.com>
2018-09-20 14:37:34 +0800

    something fot wechat

--- resources/views/forum/edit.blade.php
+++ resources/views/forum/edit.blade.php
@@ -13,7 +13,7 @@
                 <div class="form-group">
                     <div class="editor">
                         <label for="body">内容:</label>
-                        <textarea name="body" id="body"  cols="30" rows="10" class="form-control" >{{ $$discussion->body }}</textarea>
+                        <textarea name="body" id="body"  cols="30" rows="10" class="form-control" ></textarea>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary pull-right">更新帖子</button>
