7e12c1bcc0ebd0e3aa5812902e227aa14ccafdaa
Flourishing827 <huangxiangrong827@163.com>
2018-09-22 01:58:10 +0800

    something fot wechat

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($password)
     {
-        $this->attributes['password'] = \Hash::make($password);
+        $this->attributes['password'] = bcrypt($password);
     }
 
     public static function register(array $attributes)
