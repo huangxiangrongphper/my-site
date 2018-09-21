501e9357775ede53cb3022ec7aa769217dffc827
Flourishing827 <huangxiangrong827@163.com>
2018-09-21 22:00:28 +0800

    something fot wechat

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -178,6 +178,7 @@ class UsersController extends Controller
         if($request->isMethod('get')){
             $confirm_code  = $request->input('token');
             session('confirm_code ',$confirm_code);
+            dd(session('confirm_code'));
             return view('users.passwordReset');
         }else{
              $request->validate([
@@ -185,8 +186,6 @@ class UsersController extends Controller
                 'password' => 'required|min:6|confirmed',
                 'password_confirmation' => 'required|min:6',
             ]);
-            $confirm_code = session('confirm_code');
-            dd($confirm_code);
             $email         = $request->get('email');
             $user_info = User::where(function($query) use($email,$confirm_code) {
                 $query->where('email',$email)
