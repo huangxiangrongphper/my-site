31e3186da4d9ec5555c0257f9133d800356d2cf1
Flourishing827 <huangxiangrong827@163.com>
2018-09-21 19:52:22 +0800

    something fot wechat

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -143,7 +143,7 @@ class UsersController extends Controller
 
             $user_email = User::where(function($query) use($email) {
                 $query->where('email',$email)
-                    ->andWhere('is_confirmed','1');
+                    ->where('is_confirmed',1);
             })->get();
 
             dd($request->$user_email);
