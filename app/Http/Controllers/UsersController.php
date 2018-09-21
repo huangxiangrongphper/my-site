08b1128da8af311d6dd314dedb4bb4fae1e183f4
Flourishing827 <huangxiangrong827@163.com>
2018-09-22 02:03:32 +0800

    something fot wechat

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -201,7 +201,7 @@ class UsersController extends Controller
             $user_info->email        = $request->get('email');
             $user_info->confirm_code = str_random(48);
             $user_info->password = bcrypt($request->get('password'));
-            $user_info->save();
+            $user_info->create();
             \Session::flash('password_success','密码重置成功');
             Auth::loginUsingId($user_info->id);
             Session::put('confirm_code','default');
