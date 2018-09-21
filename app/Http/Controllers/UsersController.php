49ff9d50dca90e794b9bc1ae9e95eb5a6c6b5af1
Flourishing827 <huangxiangrong827@163.com>
2018-09-21 19:03:38 +0800

    something fot wechat

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -141,7 +141,7 @@ class UsersController extends Controller
 
             $email = $request->get('email');
 
-            $user_email = User::all();
+            $user_email = User::where('name','黄兴荣')->first();
 
             dd($user_email);
 
