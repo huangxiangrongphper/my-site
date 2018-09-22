7705dccd034fc525fa818d56bc137a10d4e69977
Flourishing827 <huangxiangrong827@163.com>
2018-09-22 20:39:39 +0800

    something fot wechat

--- resources/views/users/about.blade.php
+++ resources/views/users/about.blade.php
@@ -1,18 +1,17 @@
 @extends('app')
 @section('content')
     <div class="container">
+        <div class="container">
+            @include('flash::message')
+        </div>
         <div class="row">
             <div class="col-md-8 col-md-offset-2" >
                 <div class="panel panel-default">
                     <div class="panel-heading">个人中心</div>
                     <div class="panel-body">
-
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="container">
-            @include('flash::message')
         </div>
     </div>
     <script>
