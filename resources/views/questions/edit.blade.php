741feed2ab43819c3ab8e29dca36841b42bdfa99
Flourishing827 <huangxiangrong827@163.com>
2018-09-23 18:11:17 +0800

    something fot wechat

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2" >
                 <div class="panel panel-default">
-                    <div class="panel-heading">发布问题</div>
+                    <div class="panel-heading">编辑问题</div>
                     <div class="panel-body">
                         <form action="/questions/{{ $question->id }}" method="post">
                             {{ method_field('PATCH') }}
