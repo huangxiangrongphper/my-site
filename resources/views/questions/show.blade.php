0982d982dc3cfdaee6b3b20a606e328f1b80edb6
Flourishing827 <huangxiangrong827@163.com>
2018-09-24 01:05:48 +0800

    something fot wechat

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -13,7 +13,7 @@
                         @endforeach
                         @endif
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body content">
                         {!! $question->body !!}
                     </div>
                     <div class="actions">
@@ -42,7 +42,7 @@
                                         <img src="{{ $question->user->avatar }}" alt="64x64" class="media-object img-circle" style="width: 36px;height: 36px">
                                     </a>
                                 </div>
-                                <div class="media-body" style="height: 200px">
+                                <div class="media-body" >
                                     <h4 class="media-heading">
                                         <a href="/user/{{ $answers->user->name }}">
                                             {{ $answers->user->name }}
