ce72522393fcafdfe86140db8f4cafcbb6bf771b
Flourishing827 <huangxiangrong827@163.com>
2018-09-24 14:40:24 +0800

    something fot wechat

--- app/Http/Controllers/QuestionFollowController.php
+++ app/Http/Controllers/QuestionFollowController.php
@@ -9,7 +9,7 @@ class QuestionFollowController extends Controller
 {
     public function follow($question)
     {
-        dd(Auth::user());
+        
         Auth::user()->follows($question);
 
         return back();
