a79d558638e81d8748d72373953c5e365d7fc2f3
Flourishing827 <huangxiangrong827@163.com>
2018-09-24 17:41:16 +0800

    something fot wechat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>hellohxr.cn</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="/css/font-awesome.css">
     <link rel="stylesheet" href="/css/select2.min.css">
@@ -13,7 +14,6 @@
     <script src="/js/jquery.form.js"></script>
     <script src="/js/select2.min.js"></script>
     <script src="/js/app.js"></script>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <nav class="navbar navbar-default navbar-static-top">
