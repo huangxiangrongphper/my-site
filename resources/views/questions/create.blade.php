fb74b13a60b0e8dd6a9a5a3b3934b37c2630921d
Flourishing827 <huangxiangrong827@163.com>
2018-09-25 20:45:58 +0800

    something fot wechat

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section('content')
     @include('vendor.ueditor.assets')
-    <div class="container" id="app">
+    <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2" >
                <div class="panel panel-default">
