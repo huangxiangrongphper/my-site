f5e9700c0524ad31204495b6d5fb70f4146e5e09
Flourishing827 <huangxiangrong827@163.com>
2018-09-26 00:10:46 +0800

    something fot wechat

--- resources/js/components/UserFollowButton.vue
+++ resources/js/components/UserFollowButton.vue
@@ -11,7 +11,7 @@
     export default {
         props:['user'],
         mounted() {
-            axios.get('/api/user/followers',{'user':this.user}).then(response => {
+            axios.get('/api/user/followers/' + this.user).then(response => {
                 this.followed = response.data.followed
             })
         },
@@ -27,7 +27,7 @@
         },
         methods:{
             follow() {
-                axios.post('/api/user/follow/'+ this.user).then(response => {
+                axios.post('/api/user/follow',{'user':this.user}).then(response => {
                     this.followed = response.data.followed
                 })
             }
