3c8f33e2c7b73aa78deb51255b964248565accdb
Flourishing827 <huangxiangrong827@163.com>
2018-09-27 18:59:12 +0800

    something fot wechat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <title>hellohxr.cn</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="apiToken" content="{{ Auth::check() ? 'Bearer '.Auth::user()->api_token : 'Bearer ' }}">
-    <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="/css/font-awesome.css">
     <link rel="stylesheet" href="/css/select2.min.css">
     <link rel="stylesheet" href="/css/style.css">
