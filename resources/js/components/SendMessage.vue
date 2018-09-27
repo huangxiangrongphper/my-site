a9971f9a4adbe4aac70ac3d7391292429a83408b
Flourishing827 <huangxiangrong827@163.com>
2018-09-27 17:56:04 +0800

    something fot wechat

--- resources/js/components/SendMessage.vue
+++ resources/js/components/SendMessage.vue
@@ -1,31 +1,32 @@
 <template>
     <div>
-    <button
-            class="btn btn-default"
-            v-bind:class="{'btn-primary' : voted}"
-            @click="showSendMessageForm"
-    >发送私信</button>
+        <button
+                class="btn btn-default pull-right"
+                style="margin-top:-36px;"
+                @click="showSendMessageForm"
+        >发送私信</button>
         <div class="modal fade" id="modal-send-message" tabindex="-1" role="dialog">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-
                         <h4 class="modal-title">
                             发送私信
                         </h4>
+
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
 
                     <div class="modal-body">
-                        <textarea name="body" class="form-control"></textarea>
+                        <textarea name="body" class="form-control" v-model="body" v-if="!status"></textarea>
+                        <div class="alert alert-success" v-if="status">
+                            <strong>私信发送成功</strong>
+                        </div>
                     </div>
-
-                    <!-- Modal Actions -->
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
                         <button type="button" class="btn btn-primary" @click="store">
                             发送私信
                         </button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
                     </div>
                 </div>
             </div>
