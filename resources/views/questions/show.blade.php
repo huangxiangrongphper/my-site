579c0fbf5544f39a7bbb3dbfff4c436288014e26
Flourishing827 <huangxiangrong827@163.com>
2018-09-28 00:45:13 +0800

    something fot wechat

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -115,9 +115,9 @@
                         @else
                         <a href="{{url('user/login')}}" class="btn btn-default">关注他</a>
                         @endif
-                        @if(Auth::check() && $question->user_id == optional(Auth::guard('api')->user()->id))
+                        @if(Auth::check() && $question->user_id == optional(Auth::id))
                         <send-message user="{{ $question->user_id }}"></send-message>
-                        @elseif($question->user_id  == optional(Auth::guard('api')->user()->id))
+                        @elseif($question->user_id  == optional(Auth::id))
                         @else
                         <a href="{{url('user/login')}}" class="btn btn-default">发送私信</a>
                         @endif
