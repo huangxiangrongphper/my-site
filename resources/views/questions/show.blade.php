9cb10fb503ad0d6882b15d0b43a68ace1f29ed32
Flourishing827 <huangxiangrong827@163.com>
2018-09-27 20:21:34 +0800

    something fot wechat

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,5 +1,55 @@
-@extends('app')
-@section('content')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hellohxr.cn</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="apiToken" content="{{ Auth::check() ? 'Bearer '.Auth::user()->api_token : 'Bearer ' }}">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="/js/jquery-2.1.4.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/font-awesome.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">HELLOHXR.CN</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/">首页</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                @if(Auth::check())
+                    <li><a id="drop1" type="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle" data-toggle="dropdown" role="button" href="#">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dLabel">
+                            <li><a href="/user/avatar"> <i class="fa fa-user"></i> 更换头像</a></li>
+                            <li><a href="#"> <i class="fa fa-cog"></i> 更换密码</a></li>
+                            <li><a href="/about"> <i class="fa fa-heart"></i> 个人中心</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li> <a href="/logout">  <i class="fa fa-sign-out"></i> 退出登录</a></li>
+                        </ul>
+                    </li>
+                    <li><img src="{{Auth::user()->avatar}}" class="img-circle" width="50" alt=""></li>
+                @else
+                    <li><a href="/user/login">登 录</a></li>
+                    <li><a href="/user/register">注 册</a></li>
+                @endif
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
     @include('vendor.ueditor.assets')
     <div class="container" id="app" >
         <div class="row">
@@ -154,4 +204,5 @@
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
     </script>
-@stop
+</body>
+</html>
