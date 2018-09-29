2ad80d25b7e733059d0521c32562c77932871a6f
Flourishing827 <huangxiangrong827@163.com>
2018-09-30 01:53:57 +0800

    something fot wechat

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -21,7 +21,9 @@ class InboxController extends Controller
         $messages = Message::where('to_user_id',user()->id)
             ->orWhere('from_user_id',user()->id)
             ->with(['fromUser','toUser'])->get();
-        
+
+        return $messages->groupBy('to_user_id');
+
         return view('inbox.index',['messages' => $messages->groupBy('to_user_id') ]);
     }
 
