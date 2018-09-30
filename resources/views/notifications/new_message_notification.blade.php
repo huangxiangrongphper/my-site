469c9a74d4cd275b96dd25b96936fe1752057dbd
Flourishing827 <huangxiangrong827@163.com>
2018-09-30 22:35:41 +0800

    something fot wechat

--- resources/views/notifications/new_message_notification.blade.php
+++ resources/views/notifications/new_message_notification.blade.php
@@ -1,5 +1,5 @@
 <li class="notifications {{ $notification->unread() ? 'unread' : '' }}">
-    <a href="/inbox/{{ $notification->data['dialog'] }}">
+    <a href="{{$notification->unread() ? '/notifications/'.$notification->id.'?redirect_url=/inbox/'.$notification->data['dialog'] : '/inbox/'.$notification->data['dialog']}}">
         {{ $notification->data['name'] }} 给你发了一条私信🙈
     </a>
 </li>
