217369c94a6871ec91a32d70139f4cb02ce67d62
Flourishing827 <huangxiangrong827@163.com>
2018-09-30 20:27:35 +0800

    something fot wechat

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -129,10 +129,10 @@
                         @else
                         <a href="{{url('user/login')}}" class="btn btn-default">关注他</a>
                         @endif
-                        @if(Auth::check())
+                        @if(Auth::check() && user()->id != $question->user_id)
                         <send-message user="{{ $question->user_id }}" ></send-message>
                         @else
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="{{url('user/login')}}" class="btn btn-default">发送私信</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    <a href="{{url('user/login')}}" class="btn btn-default">发送私信</a>
                         @endif
                     </div>
                 </div>
