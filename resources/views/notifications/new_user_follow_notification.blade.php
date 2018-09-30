f14cfadadf6ece44560edc40d35eb3c813875906
Flourishing827 <huangxiangrong827@163.com>
2018-10-01 00:40:22 +0800

    something fot wechat

--- resources/views/notifications/new_user_follow_notification.blade.php
+++ resources/views/notifications/new_user_follow_notification.blade.php
@@ -1,5 +1,5 @@
 <li class="notifications {{ $notification->unread() ? 'unread' : '' }}">
     <a href="/notifications/{{$notification->id}}/register">点击标示已读</a>
         {{ $notification->data['name'] }}
-    </a>关注了你.👏
+    </a>关注了你.👏 <span class="pull-right">{{ $notification->created_at->format('Y-m-d H:i:s') }}</span>
 </li>
