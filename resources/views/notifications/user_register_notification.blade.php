feeb956b234e17bb7971c94a824ce293ef0deb95
Flourishing827 <huangxiangrong827@163.com>
2018-10-01 00:41:32 +0800

    something fot wechat

--- resources/views/notifications/user_register_notification.blade.php
+++ resources/views/notifications/user_register_notification.blade.php
@@ -1,4 +1,4 @@
 <li class="notifications {{ $notification->unread() ? 'unread' : '' }}">
     <a href="/notifications/{{$notification->id}}/register">点击标示已读</a>
-       亲爱的 :{{ $notification->data['name'] }}😘 恭喜您成为本站新的用户,在这里你可以和大家一起探讨各种PHP知识😋
+       亲爱的 :{{ $notification->data['name'] }}😘 恭喜您成为本站新的用户,在这里你可以和大家一起探讨各种PHP知识😋 <span class="pull-right">{{ $notification->created_at->format('Y-m-d H:i:s') }}</span>
 </li>
