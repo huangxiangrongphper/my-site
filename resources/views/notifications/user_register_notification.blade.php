061f5662f23d971f060217ac5c7c5282fa494bac
Flourishing827 <huangxiangrong827@163.com>
2018-10-01 00:33:29 +0800

    something fot wechat

--- resources/views/notifications/user_register_notification.blade.php
+++ resources/views/notifications/user_register_notification.blade.php
@@ -1,5 +1,4 @@
 <li class="notifications {{ $notification->unread() ? 'unread' : '' }}">
-
-       亲爱的 :{{ $notification->data['name'] }}😘 恭喜您成为本站新的用户,在这里你可以和大家一起探讨各种PHP知识😋
     <a href="/notifications/{{$notification->id}}/register">点击标示已读</a>
+       亲爱的 :{{ $notification->data['name'] }}😘 恭喜您成为本站新的用户,在这里你可以和大家一起探讨各种PHP知识😋
 </li>
