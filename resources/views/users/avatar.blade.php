84ca3514a6b8788d0569e037be033d281b3b67e2
Flourishing827 <huangxiangrong827@163.com>
2018-10-01 15:00:50 +0800

    something fot wechat

--- resources/views/users/avatar.blade.php
+++ resources/views/users/avatar.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">更换头像</div>
-                    <div class="panel-body user-avatar">
+                    <div class="panel-body content">
                         <user-avatar avatar="{{ Auth::user()->avatar }}"></user-avatar>
                     </div>
                 </div>
